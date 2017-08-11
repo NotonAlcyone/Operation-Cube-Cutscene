0c19b1adec92a5b7ed115ac21c47f0d7e4e37dcf
Lee Dongyun <notonalcyone@gmail.com>
2017-08-12 00:13:04 +0900

    Framework commit

---
+++ Stat.py
@@ -0,0 +1,53 @@
+##############################################
+canvasSizeX = 1024	
+canvasSizeY = 576
+canvasCenterX = canvasSizeX / 2
+canvasCenterY = canvasSizeY / 2
+
+
+speechBarPositionX = canvasCenterX #대사창 위치
+speechBarPositionY = 480
+
+namePostionX = 230 #이름 위치
+namePostionY = 425
+
+textPositionX = 200 #대사 위치
+textPositionY = 455
+
+
+barPath = "bar_crop.png"
+buttonPath = "Button.png"
+nameBarPath = "Bar_Name.png"
+imageFolder = "image/"
+voiceFolder = "voice/"
+
+nameFont = "Arial 15 bold"
+textFont = "mssansseif 14"
+uiFont = "Courier 10"
+
+encounterSpace = 130 #선택지 이미지 파일간 거리
+
+textTypeSpeed = 0.05 #대사 타이핑 속도
+
+letterSpace = 17 #자간
+lineHeight = 27 #행간
+
+voiceVolume = 10 #음성 볼륨
+
+saveButton = False
+nameBarStat = False
+
+nameFontColor = 'white'
+textFontColor = 'white'
+
+autoLineAddMove = False;
+
+titleBGM = False
+ingameBGM = False
+titleBGMPath = "titletheme.wav"
+BGMVolume = 3
+
+specialWord = ["!", ",", "?", ".", "~",'"'] #특수문자 목록
+
+
+##############################################
